b2b9f7f0cd0a756949168e20349be2f1d78e7275
training@hashiba.org <training@hashiba.org>
2017-11-14 04:28:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const drivers =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+destructivelyAppendDriver('Ralph');
+drivers.pop();
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+
+}
+
+console.log("the size of drivers after destructivelyPrependDriver is "+ drivers.length);
+console.log("drivers[]: " + drivers);
+destructivelyPrependDriver('Bob');
+drivers.shift();
+
+function destructivelyRemoveLastDriver(){
+drivers.pop()
+}
+
+destructivelyRemoveLastDriver();
+
+console.log("the size of drivers after destructivelyRemoveLastDriver is "+ drivers.length);
+console.log("drivers[]: " + drivers);
